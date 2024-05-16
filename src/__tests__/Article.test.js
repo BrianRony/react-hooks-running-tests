68bf50f1c5c62a2441c457c8c8d153b2caaa0d71
Brian Ngetich <kipronobrian97@gmail.com>
2024-05-16 03:56:52 +0300

    Tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,9 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  ////Display content on Node as it would have displayed on DOM
+  // const element = screen.queryByText("please pass this test");
+  // screen.debug(element);
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
